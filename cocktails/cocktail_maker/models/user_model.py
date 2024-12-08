d88ae94df328051470aa94932f48de42cca039b4
Malvika <140577395+malvikad31@users.noreply.github.com>
2024-12-08 11:46:35 -0500

    Update user_model.py

--- cocktails/cocktail_maker/models/user_model.py
+++ cocktails/cocktail_maker/models/user_model.py
@@ -4,8 +4,8 @@ import os
 
 from sqlalchemy.exc import IntegrityError
 
-from meal_max.db import db
-from meal_max.utils.logger import configure_logger
+from cocktail_maker.db import db
+from cocktail_maker.utils.logger import configure_logger
 
 
 logger = logging.getLogger(__name__)
